84a883d5444fa74155166dd29b625d23f12bbd6b
konruvikt <114674272+konruvikt@users.noreply.github.com>
2022-11-23 16:06:33 +0300

    first commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,53 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    @OneToOne (mappedBy = "car")
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
